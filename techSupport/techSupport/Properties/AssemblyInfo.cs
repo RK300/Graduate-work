bf74552dafe66eeb37126adc415b8a81e2c5ba3e
Alexey Medovskiy <alekseymedovskiy@gmail.com>
2023-06-12 20:59:09 +0300

    version 12.06.2023 x2

--- techSupport/techSupport/Properties/AssemblyInfo.cs
+++ techSupport/techSupport/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("techSupport")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CRM Everest")]
+[assembly: AssemblyDescription("The Everest Customer Relationship Management System is an application software for organizations designed to automate customer interaction strategies, in particular, to increase sales, optimize marketing and improve customer service by storing customer information.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("techSupport")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Medovskiy incorporated")]
+[assembly: AssemblyProduct("CRM Everest")]
+[assembly: AssemblyCopyright("Copyright Alexey Medovskiy ©  2023")]
+[assembly: AssemblyTrademark("Medovskiy incorporated")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
